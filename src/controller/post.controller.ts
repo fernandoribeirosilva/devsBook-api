021667f46f0f39c25899c28780faa733031d9348
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-09-26 09:33:36 -0300

    criado função de like

--- src/controller/post.controller.ts
+++ src/controller/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { CreatePostService } from "../services/post/create.post.service";
+import { PostLiked } from "../services/post/postLiked.service";
 
 class PostController {
   async newPost(req: Request, res: Response) {
@@ -14,6 +15,19 @@ class PostController {
     } catch (error: InstanceType<Error>) {
       res.status(400).json({ error: error.message });
       return;
+    }
+  }
+
+  async like(req: Request, res: Response) {
+    try {
+      const loggedUser = res.locals.loggedUser;
+      const { id } = req.params;
+
+      const like = PostLiked.liked(+id, loggedUser.id);
+      return res.status(200).json({ like })
+
+    } catch (error: InstanceType<Error>) {
+      return res.status(404).json({ error: error.message });
     }
   }
 }
